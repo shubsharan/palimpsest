249ae6c665e5ae3afaa7af6a43463a216b611b5a
Shubhankar Sharan <145727560+shubsharan@users.noreply.github.com>
2026-08-06 14:16:39 -0700

    fix fonts

--- src/viewer/ui/main.tsx
+++ src/viewer/ui/main.tsx
@@ -1,4 +1,7 @@
 import "@fontsource-variable/newsreader/wght.css";
+import "@fontsource/ibm-plex-sans/latin-400.css";
+import "@fontsource/ibm-plex-sans/latin-500.css";
+import "@fontsource/ibm-plex-sans/latin-600.css";
 import "@fontsource/ibm-plex-mono/latin-400.css";
 import "@fontsource/ibm-plex-mono/latin-500.css";
 import "@fontsource/ibm-plex-mono/latin-600.css";
